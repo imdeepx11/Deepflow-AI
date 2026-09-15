e8665c74c6cff70fb7261a15c1268848e806c176
Deepak Gupta <mrdeepak.g11@gmail.com>
2026-09-15 12:13:01 +0530

    Add root main.py entry point

---
+++ main.py
@@ -0,0 +1,7 @@
+import os
+import sys
+import uvicorn
+
+if __name__ == '__main__':
+    sys.path.append(os.path.join(os.path.dirname(__file__), 'backend'))
+    uvicorn.run('app.main:app', host='127.0.0.1', port=8000, reload=True, app_dir='backend')
